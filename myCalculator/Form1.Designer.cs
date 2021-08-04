bbc5d24c6900badde63d44d7d64cb564b0f5e6fa
mohammad <muhammad.mohammadi1377@gmail.com>
2021-08-04 20:01:55 +0430

    finish my calculatro Project

--- myCalculator/Form1.Designer.cs
+++ myCalculator/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace myCalculator
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.btn7 = new System.Windows.Forms.Button();
@@ -49,29 +50,34 @@ namespace myCalculator
             this.btnSum = new System.Windows.Forms.Button();
             this.btnEqual = new System.Windows.Forms.Button();
             this.btnCleanAll = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(5, 7);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(314, 29);
+            this.textBox1.Size = new System.Drawing.Size(302, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(5, 42);
+            this.textBox2.Location = new System.Drawing.Point(12, 90);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(314, 115);
+            this.textBox2.Size = new System.Drawing.Size(302, 115);
             this.textBox2.TabIndex = 0;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
             // 
             // btn7
             // 
-            this.btn7.Location = new System.Drawing.Point(17, 163);
+            this.btn7.Location = new System.Drawing.Point(12, 211);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(71, 82);
             this.btn7.TabIndex = 1;
@@ -81,7 +87,7 @@ namespace myCalculator
             // 
             // btn8
             // 
-            this.btn8.Location = new System.Drawing.Point(94, 163);
+            this.btn8.Location = new System.Drawing.Point(89, 211);
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(71, 82);
             this.btn8.TabIndex = 1;
@@ -91,7 +97,7 @@ namespace myCalculator
             // 
             // btn9
             // 
-            this.btn9.Location = new System.Drawing.Point(171, 163);
+            this.btn9.Location = new System.Drawing.Point(166, 211);
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(71, 82);
             this.btn9.TabIndex = 1;
@@ -101,7 +107,7 @@ namespace myCalculator
             // 
             // btnDivision
             // 
-            this.btnDivision.Location = new System.Drawing.Point(248, 163);
+            this.btnDivision.Location = new System.Drawing.Point(243, 211);
             this.btnDivision.Name = "btnDivision";
             this.btnDivision.Size = new System.Drawing.Size(71, 82);
             this.btnDivision.TabIndex = 1;
@@ -111,7 +117,7 @@ namespace myCalculator
             // 
             // btn4
             // 
-            this.btn4.Location = new System.Drawing.Point(17, 251);
+            this.btn4.Location = new System.Drawing.Point(12, 299);
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(71, 82);
             this.btn4.TabIndex = 1;
@@ -121,7 +127,7 @@ namespace myCalculator
             // 
             // btn5
             // 
-            this.btn5.Location = new System.Drawing.Point(94, 251);
+            this.btn5.Location = new System.Drawing.Point(89, 299);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(71, 82);
             this.btn5.TabIndex = 1;
@@ -131,7 +137,7 @@ namespace myCalculator
             // 
             // btn6
             // 
-            this.btn6.Location = new System.Drawing.Point(171, 251);
+            this.btn6.Location = new System.Drawing.Point(166, 299);
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(71, 82);
             this.btn6.TabIndex = 1;
@@ -141,7 +147,7 @@ namespace myCalculator
             // 
             // btnMultiplication
             // 
-            this.btnMultiplication.Location = new System.Drawing.Point(248, 251);
+            this.btnMultiplication.Location = new System.Drawing.Point(243, 299);
             this.btnMultiplication.Name = "btnMultiplication";
             this.btnMultiplication.Size = new System.Drawing.Size(71, 82);
             this.btnMultiplication.TabIndex = 1;
@@ -151,7 +157,7 @@ namespace myCalculator
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(17, 339);
+            this.btn1.Location = new System.Drawing.Point(12, 387);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(71, 82);
             this.btn1.TabIndex = 1;
@@ -161,7 +167,7 @@ namespace myCalculator
             // 
             // btn2
             // 
-            this.btn2.Location = new System.Drawing.Point(94, 339);
+            this.btn2.Location = new System.Drawing.Point(89, 387);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(71, 82);
             this.btn2.TabIndex = 1;
@@ -171,7 +177,7 @@ namespace myCalculator
             // 
             // btn3
             // 
-            this.btn3.Location = new System.Drawing.Point(171, 339);
+            this.btn3.Location = new System.Drawing.Point(166, 387);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(71, 82);
             this.btn3.TabIndex = 1;
@@ -181,7 +187,7 @@ namespace myCalculator
             // 
             // btnMinus
             // 
-            this.btnMinus.Location = new System.Drawing.Point(248, 339);
+            this.btnMinus.Location = new System.Drawing.Point(243, 387);
             this.btnMinus.Name = "btnMinus";
             this.btnMinus.Size = new System.Drawing.Size(71, 82);
             this.btnMinus.TabIndex = 1;
@@ -191,7 +197,7 @@ namespace myCalculator
             // 
             // btnDat
             // 
-            this.btnDat.Location = new System.Drawing.Point(17, 427);
+            this.btnDat.Location = new System.Drawing.Point(12, 475);
             this.btnDat.Name = "btnDat";
             this.btnDat.Size = new System.Drawing.Size(71, 82);
             this.btnDat.TabIndex = 1;
@@ -202,7 +208,7 @@ namespace myCalculator
             // 
             // btnZero
             // 
-            this.btnZero.Location = new System.Drawing.Point(94, 427);
+            this.btnZero.Location = new System.Drawing.Point(89, 475);
             this.btnZero.Name = "btnZero";
             this.btnZero.Size = new System.Drawing.Size(71, 82);
             this.btnZero.TabIndex = 1;
@@ -212,7 +218,7 @@ namespace myCalculator
             // 
             // btnClean
             // 
-            this.btnClean.Location = new System.Drawing.Point(171, 427);
+            this.btnClean.Location = new System.Drawing.Point(166, 475);
             this.btnClean.Name = "btnClean";
             this.btnClean.Size = new System.Drawing.Size(71, 82);
             this.btnClean.TabIndex = 1;
@@ -222,7 +228,7 @@ namespace myCalculator
             // 
             // btnSum
             // 
-            this.btnSum.Location = new System.Drawing.Point(248, 427);
+            this.btnSum.Location = new System.Drawing.Point(243, 475);
             this.btnSum.Name = "btnSum";
             this.btnSum.Size = new System.Drawing.Size(71, 82);
             this.btnSum.TabIndex = 1;
@@ -232,17 +238,17 @@ namespace myCalculator
             // 
             // btnEqual
             // 
-            this.btnEqual.Location = new System.Drawing.Point(12, 515);
+            this.btnEqual.Location = new System.Drawing.Point(12, 563);
             this.btnEqual.Name = "btnEqual";
-            this.btnEqual.Size = new System.Drawing.Size(230, 61);
-            this.btnEqual.TabIndex = 2;
+            this.btnEqual.Size = new System.Drawing.Size(225, 61);
+            this.btnEqual.TabIndex = 1;
             this.btnEqual.Text = "=";
             this.btnEqual.UseVisualStyleBackColor = true;
             this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
             // 
             // btnCleanAll
             // 
-            this.btnCleanAll.Location = new System.Drawing.Point(248, 515);
+            this.btnCleanAll.Location = new System.Drawing.Point(243, 563);
             this.btnCleanAll.Name = "btnCleanAll";
             this.btnCleanAll.Size = new System.Drawing.Size(71, 61);
             this.btnCleanAll.TabIndex = 1;
@@ -250,11 +256,33 @@ namespace myCalculator
             this.btnCleanAll.UseVisualStyleBackColor = true;
             this.btnCleanAll.Click += new System.EventHandler(this.btnCleanAll_Click);
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(334, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(89, 22);
+            this.toolStripButton1.Text = "change Theme";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 582);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(334, 636);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.btnEqual);
             this.Controls.Add(this.btnSum);
             this.Controls.Add(this.btnMinus);
@@ -278,9 +306,13 @@ namespace myCalculator
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculator";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -308,6 +340,8 @@ namespace myCalculator
         private System.Windows.Forms.Button btnSum;
         private System.Windows.Forms.Button btnEqual;
         private System.Windows.Forms.Button btnCleanAll;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
     }
 }
 
--- myCalculator/Form1.cs
+++ myCalculator/Form1.cs
@@ -12,12 +12,20 @@ namespace myCalculator
 {
     public partial class Form1 : Form
     {
+        #region variable
         float result = 0;
         float _operand1 = 0;
         char _operator = ' ';
+        int _intForTextbox1 = 0;
+        bool chengeTheme = true;
+        #endregion
+
         public Form1()
         {
+            
             InitializeComponent();
+            //toolStrip1.Renderer = new MySR();
+
         }
         #region click btn number code
 
@@ -118,6 +126,12 @@ namespace myCalculator
         private void btnCleanAll_Click(object sender, EventArgs e)
         {
             textBox2.Text = "";
+            _intForTextbox1 = 0;
+            textBox1.Text = "";
+            _operand1 = 0;
+            _operator = ' ';
+
+
         }
 
         #endregion
@@ -127,83 +141,305 @@ namespace myCalculator
         #region click btn Operators
         private void btnSum_Click(object sender, EventArgs e)
         {
-            _operand1 = float.Parse(textBox2.Text);
-            textBox2.Text = "";
-            _operator = '+';
+            if (textBox2.Text != "")
+            {
+                if (_operator != ' ')
+                {
+                    ifclickEnotherbtnEual();
+                }
+                else if (textBox2.Text != "+" & textBox2.Text != "-")
+                {
+
+                    _operand1 = float.Parse(textBox2.Text);
+                    _operator = '+';
+                    textBox2.Text = "";
+
+
+                }
+
+
+            }
+            else
+            {
+                textBox2.Text = "+";
+            }
+
         }
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
-            _operand1 = float.Parse(textBox2.Text);
-            textBox2.Text = "";
-            _operator = '-';
+            if (textBox2.Text != "")
+            {
+                if (_operator != ' ')
+                {
+                    ifclickEnotherbtnEual();
+                }
+                else if (textBox2.Text != "-" && textBox2.Text != "+")
+                {
+
+
+                    _operand1 = float.Parse(textBox2.Text);
+
+
+                    _operator = '-';
+
+                    textBox2.Text = "";
+                }
+
+
+            }
+            else
+            {
+                textBox2.Text = "-";
+            }
+
+
         }
 
         private void btnMultiplication_Click(object sender, EventArgs e)
         {
-            _operand1 = float.Parse(textBox2.Text);
-            textBox2.Text = "";
+            if (_operator != ' ')
+            {
+                ifclickEnotherbtnEual();
+            }
+            else
+            {
+                if (textBox2.Text != "")
+                    _operand1 = float.Parse(textBox2.Text);
+
+
+            }
             _operator = '*';
+            textBox2.Text = "";
         }
 
         private void btnDivision_Click(object sender, EventArgs e)
         {
-            _operand1 = float.Parse(textBox2.Text);
-            textBox2.Text = "";
-            _operator = '/';
-        }
-        #endregion
-
-        private void btnEqual_Click(object sender, EventArgs e)
-        {   
-            float _operand2 = 0;
-            if (textBox2.Text== "")
+            if (_operator != ' ')
             {
-                _operand2 = 0;
-               
+                ifclickEnotherbtnEual();
             }
             else
             {
+                if (textBox2.Text != "")
+                {
+                    _operand1 = float.Parse(textBox2.Text);
+                }
+
+
+
+            }
+            _operator = '/';
+            textBox2.Text = "";
+        }
+        #endregion
+
+
+        #region click btn equal
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+
+            float _operand2 = 0;
+            if (textBox2.Text == "" || textBox2.Text == "-" || textBox2.Text == "+")
+            {
+                textBox2.Text = "0";
+                _operand2 = 0;
+
+            }
+            else
+            {
+
                 _operand2 = float.Parse(textBox2.Text);
             }
-           
+
 
             switch (_operator)
             {
                 case '+':
-                    result = _operand1 + _operand2;
-                    break;
+                    {
+                        txtTextbox1();
+                        result = _operand1 + _operand2;
+                        break;
+                    }
+
                 case '-':
-                    result = _operand1 - _operand2;
-                    break;
+                    {
+                        txtTextbox1();
+                        result = _operand1 - _operand2;
+                        break;
+                    }
                 case '*':
-                    result = _operand1 * _operand2;
-                    break;
+                    {
+                        txtTextbox1();
+                        result = _operand1 * _operand2;
+                        break;
+                    }
                 case '/':
                     {
-                        if(_operand2!= 0)
+                        if (_operand2 != 0)
                         {
                             result = _operand1 / _operand2;
+                            txtTextbox1();
+                            textBox2.Text = result.ToString();
+                            _operator = ' ';
                         }
-                        else
+                        else if (_operand2 == 0)
                         {
-                            MessageBox.Show("", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            float x = _operand1;
+                            MessageBox.Show("can not be divided by Zero", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            textBox2.Text = "";
+                            _operand1 = x;
+                            _operator = '/';
+
                         }
-                        
+
                         break;
 
                     }
-              
-               }
 
-            if(_operator!=' ')
+            }
+
+            if (_operator != ' ' && _operator != '/')
             {
                 textBox2.Text = result.ToString();
+                _operator = ' ';
             }
-           
-           
+
+
             result = 0;
-            _operator = ' ';
+
+        }
+        #endregion
+
+
+
+
+        #region unable press somekeys
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) & !char.IsControl(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+            }
+        }
+        #endregion
+
+
+
+        #region help metods
+        private void ifclickEnotherbtnEual()
+        {
+            if (textBox2.Text != "" && textBox2.Text != "+" && textBox2.Text != "-")
+            {
+                switch (_operator)
+                {
+                    case '+':
+                        {
+                            txtTextbox1();
+                            _operand1 += float.Parse(textBox2.Text);
+                            textBox2.Text = "";
+                            //_operator = ' ';
+                            break;
+                        }
+                    case '-':
+                        {
+                            txtTextbox1();
+                            _operand1 -= float.Parse(textBox2.Text);
+                            textBox2.Text = "";
+                            //_operator = ' ';
+                            break;
+                        }
+                    case '*':
+                        {
+                            txtTextbox1();
+                            _operand1 *= float.Parse(textBox2.Text);
+                            textBox2.Text = "";
+                            //_operator = ' ';
+                            break;
+                        }
+                    case '/':
+                        {
+
+
+                            if (float.Parse(textBox2.Text) != 0)
+                            {
+                                txtTextbox1();
+                                _operand1 /= float.Parse(textBox2.Text);
+                                textBox2.Text = "";
+                                //_operator = ' ';
+                            }
+
+
+
+                            break;
+                        }
+
+                }
+            }
+
+        }
+
+        private void txtTextbox1()
+        {
+            if (textBox1.Text != "")
+            {
+                textBox1.Text = $"({textBox1.Text} {_operator} {textBox2.Text})";
+            }
+            else if (_intForTextbox1 == 0)
+            {
+                textBox1.Text = $"({_operand1} {_operator} {textBox2.Text})";
+                _intForTextbox1 = 1;
+            }
+            else
+            {
+                textBox1.Text = $"({textBox1.Text} {_operator} {textBox2.Text})";
+            }
+        }
+
+        #endregion
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (chengeTheme == false)
+            {
+
+                this.BackColor = Color.FromName("Control");
+                toolStrip1.BackColor = Color.FromName("Control");
+                chengeTheme = true;
+            }
+            else if (chengeTheme == true)
+            {
+                
+                this.BackColor = Color.FromName("WindowFrame");
+                toolStrip1.BackColor = Color.FromName("WindowFrame");
+                
+                chengeTheme = false;
+            }
+
+
+
+        }
+    }
+
+    public class MySR : ToolStripSystemRenderer
+    {
+        public MySR() { }
+
+        protected override void OnRenderToolStripBorder(ToolStripRenderEventArgs e)
+        {
+            base.OnRenderToolStripBorder(e);
         }
     }
 }
